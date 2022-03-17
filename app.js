63e05897284ca5eeabfe537ad7b56070a91992d2
Affou siata <affousiatao59@gmail.com>
2022-03-17 15:06:53 +0000

    autres commit

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
+// $('.boite').slick({
+//   centreMode:true,
+//   autoplay:true,
+//   infinite: false,
+//   arrows:true,
+//   slidesToShow: 4,
+//   slidesToScroll: 1,
+// });
+
+
 $('.boite').slick({
-  infinite: false,
-  slidesToShow: 4,
-  slidesToScroll: 1
-});
+  slidesToScroll:1,
+  slidesToShow:4,
+  autoplay:true,
+  
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+   {
+     breakpoint: 600,
+     settings: {
+       slidesToShow: 2,
+       slidesToScroll: 1,
+     }
+   },
+   {
+     breakpoint: 480,
+     settings: {
+       slidesToShow: 1,
+       slidesToScroll: 1
+     }
+    }
+  ]
+})
+
 $('.cartes').slick({
   infinite: false,
   slidesToShow: 1,
-  slidesToScroll: 2
+  slidesToScroll: 2,
+
 });
 
 let prev =document.querySelectorAll(".slick-prev")
